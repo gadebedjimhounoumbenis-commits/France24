9621a1e0077ca1107a49623d53950a287d07c668
gadebedjimhounoumbenis-commits <gadebedjimhounoumbenis@gmail.com>
2026-07-06 18:27:10 +0100

    Create article.dart

---
+++ lib/models/article.dart
@@ -0,0 +1,17 @@
+class Article {
+  final String title;
+  final String description;
+  final String link;
+  final String pubDate;
+  final String imageUrl;
+  final String category;
+
+  Article({
+    required this.title,
+    required this.description,
+    required this.link,
+    required this.pubDate,
+    required this.imageUrl,
+    required this.category,
+  });
+}
